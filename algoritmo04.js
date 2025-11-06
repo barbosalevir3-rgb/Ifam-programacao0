776f1a7c0f68cf05f15273a35ccb2a0be45f2be6
barbosalevir3-rgb <barbosalevir3@gmail.com>
2025-11-06 00:51:03 -0400

    Implement user input and display in algoritmo04.js

---
+++ algoritmo04.js
@@ -0,0 +1,10 @@
+let n = parseInt(prompt("Digite um numero"))
+const vetor = []
+
+for(let i = 0; i <n; i++){
+  vetor[i] prompt("")
+}
+document.writeln(<br> numeros informados o pelo usuario<br>)
+for(let i = 0; i<n; i++){
+  document.writeln(${vetor[i]})
+} 
