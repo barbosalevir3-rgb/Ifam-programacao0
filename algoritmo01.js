849b9762887c931b51ee87401b373a33de4d5576
barbosalevir3-rgb <barbosalevir3@gmail.com>
2025-11-06 00:48:21 -0400

    Create algoritmo01.js for user input sequence
    Add a script to prompt user for a number and store input.

---
+++ algoritmo01.js
@@ -0,0 +1,6 @@
+let n = parseInt(prompt("Digite um número"))
+const sequencia = []
+for(let i = 0; i<n; i++){
+   sequencia[i] = parseInt(prompt(`Digite o i{i + 1} numero`))
+}
+alert("numeros digitados: " + vetor)  
