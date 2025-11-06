f874cc31ffe0d43d5412f193f42fff2fe1171606
barbosalevir3-rgb <barbosalevir3@gmail.com>
2025-11-06 00:50:25 -0400

    Count and display even numbers from user input
    Implement logic to count and display even numbers from user input.

---
+++ algoritmo03.js
@@ -0,0 +1,12 @@
+let n = parseInt(prompt("Digite um número"))
+let pares = 0
+const vetor = []
+for(let i = 0; i<n; i++){
+  vetor[i] = prompt("")
+}
+for(let i = 0; i<n; i++){
+  if(vetor[i] % 2==0) {
+    
+  }
+}
+ document.wrilen("quantidade de numeros pares: " + pares) 
