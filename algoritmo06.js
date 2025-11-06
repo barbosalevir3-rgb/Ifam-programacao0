7e8445eb687def5cf7378fd43dfe4b4992143863
barbosalevir3-rgb <barbosalevir3@gmail.com>
2025-11-06 00:52:25 -0400

    Add item input and price calculation logic

---
+++ algoritmo06.js
@@ -0,0 +1,8 @@
+let quantidade = prompt("Quantidade de itens") 
+let total = 0
+let lista = ""
+for(let i = 1; i<=quantidade; i++){
+  let iten = prompt("digite o nome do iten");
+  let preco = parseInt(prompt("preço do item ${i}"));
+}
+alert(`${item}/npreco:${preco}/ntotal${total}`) 
